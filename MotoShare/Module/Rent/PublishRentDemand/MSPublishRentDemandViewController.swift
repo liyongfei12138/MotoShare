4878749f5b0bcfb1826c0b577a859f93e246eaa1
HouHangLei <iOSHHL@126.com>
2019-11-22 11:14:04 +0800

    no message

---
+++ MotoShare/Module/Rent/PublishRentDemand/MSPublishRentDemandViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MSPublishRentDemandViewController.swift
+//  MotoShare
+//
+//  Created by mac on 2019/11/21.
+//  Copyright © 2019 Bingo. All rights reserved.
+//
+
+import UIKit
+
+class MSPublishRentDemandViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "租赁摩托"
+    }
+}
